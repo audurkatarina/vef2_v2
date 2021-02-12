66a4de2e97bfb03402f248f9e3139f59419faf21
audurkatarina <akt6@hi.is>
2021-02-12 13:16:59 +0000

    laga ssl tengingu

--- src/db.js
+++ src/db.js
@@ -5,14 +5,17 @@ dotenv.config();
 
 const {
   DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
 } = process.env;
 
 if (!connectionString) {
   console.error('Vantar DATABASE_URL');
   process.exit(1);
 }
+// Notum SSL tengingu við gagnagrunn ef við erum *ekki* í development mode, þ.e.a.s. á local vél
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
 
-const pool = new pg.Pool({ connectionString });
+const pool = new pg.Pool({ connectionString, ssl });
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
